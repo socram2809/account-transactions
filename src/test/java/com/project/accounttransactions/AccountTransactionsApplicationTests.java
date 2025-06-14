a114c098168efef6a41597e4a07a39ca174f8423
Marcos Aurélio Macedo da Rocha Júnior <marcosau_2809@hotmail.com>
2025-06-14 10:37:43 -0300

    feat: setup project

---
+++ src/test/java/com/project/accounttransactions/AccountTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.accounttransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
